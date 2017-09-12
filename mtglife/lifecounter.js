1c80c737e95c92448378d466ed9e256c98a1af69
Mohammed Elsaadi <mohammedelsaadi@gmail.com>
2017-09-12 18:39:07 -0400

    mtglifecounter

---
+++ mtglife/lifecounter.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    if ( window.orientation == 0 || window.orientation == 180 ) { 
+        alert ('Please use your mobile device in landscape mode'); 
+    }
+};
+var cont = document.getElementById("allContainer");
